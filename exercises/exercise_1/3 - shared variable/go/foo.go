42fc93224d179a3a7729ff49505635e4242420f5
ApatShe <patricsh@stud.ntnu.no>
2026-01-07 12:55:47 +0100

    Ferdigstilte oppgave 3 på øving 1

--- exercises/exercise_1/3 - shared variable/go/foo.go
+++ exercises/exercise_1/3 - shared variable/go/foo.go
@@ -5,59 +5,52 @@ package main
 import (
 	. "fmt"
 	"runtime"
-	"time"
 )
 
 var i = 0
+var m = 0
 
-func server(ch chan int) {
-    var m int = 0
-    for {
-        select{
-            case m := <-ch:
-                if m>0 {
-                    i++
-                }else if m<0 {
-                    i--
-                }
-            case <-quit:
-                return
-        }
-    }
+func server(ch chan int, quit chan int) {
+	for {
+		select {
+		case m := <-ch:
+			i = i + m
+		case <-quit:
+			return
+		}
+	}
 }
 
-
-func incrementing(ch chan int, ch2 chan int) {
+func incrementing(ch chan int, ch2 chan int, quit chan int) {
 	//TODO: increment i 1000000 times
 	for j := 0; j < 1000000; j++ {
 		ch <- 1
 	}
-    for {
-        if <-ch2:
-            break
-    }
-    quit <- 1
+	<-ch2
+	quit <- 1
 }
 
-func decrementing(ch chan int) {
+func decrementing(ch chan int, ch2 chan int) {
 	//TODO: decrement i 1000000 times
-	for j := 0; j < 1000000; j++ {
+	for j := 0; j < 999997; j++ {
 		ch <- -1
 	}
-    ch2 <- 1
+	ch2 <- 1
 }
 
 func main() {
 	// What does GOMAXPROCS do? What happens if you set it to 1?
 	runtime.GOMAXPROCS(2)
 
-    // Initialize a channel
-    ch := make(chan int)
-    quit := make(chan int)
+	// Initialize a channel
+	ch := make(chan int)
+	ch2 := make(chan int)
+	quit := make(chan int)
 
 	// TODO: Spawn both functions as goroutines
-	go incrementing(ch)
-	go decrementing(ch)
+	go incrementing(ch, ch2, quit)
+	go decrementing(ch, ch2)
+	server(ch, quit)
 
 	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
 	// We will do it properly with channels soon. For now: Sleep.
