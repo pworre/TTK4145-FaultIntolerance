164a3acfb8c64452819308ebc1ed7e0aa32c1b97
Oskjel <oscarskjelvik@outlook.com>
2026-03-13 13:33:11 +0100

    test: add timeout test for assignOrders function

--- syncOrders/syncOrders.go
+++ syncOrders/syncOrders.go
@@ -68,8 +68,6 @@ func SynchronizationLoop(startFloor int, cfg config.Config, localStateChange cha
 				myWorldView.ElevatorState.Requests[request.Floor][request.Button].Version += 1
 			}
 
-			//assignOrders(myWorldView, peerStates, peerCabOrders, assignEvent)
-
 		case request := <-localClearing:
 			myWorldView.ElevatorState.Requests[request.Floor][request.Button].Placed = false
 			myWorldView.ElevatorState.Requests[request.Floor][request.Button].Unknown = false
